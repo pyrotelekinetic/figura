7293dd78a7d7ffb236de21940744d04fa0ff36ea
Carter Ison <carter@isons.org>
2020-12-28 15:44:02 -0700

    fix screen layout toggling

--- .screenlayout/horizontal.sh
+++ .screenlayout/horizontal.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
 xrandr --output DP-1 --off --output DP-2 --off --output DP-3 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
-export SCREENLAYOUT="horizontal"
+
 xsetbg -at 0,0 ~/dotfiles/.screenlayout/1080x1920-bang.jpg -at 1920,0 ~/dotfiles/.screenlayout/1080x1920-bang.jpg
+
+echo "horizontal" > ./current-layout
--- .screenlayout/toggle-layout.sh
+++ .screenlayout/toggle-layout.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-if [ "$SCREENLAYOUT" == "right-vertical" ]
+
+SCREENLAYOUT=$(<./current-layout)
+
+if [ $SCREENLAYOUT == "right-vertical" ]
 then
-  source ~/dotfiles/.screenlayout/horizontal.sh
-elif [ "$SCREENLAYOUT" == "horizontal" ]
+  sh ~/dotfiles/.screenlayout/horizontal.sh
+elif [ $SCREENLAYOUT == "horizontal" ]
 then
-  source ~/dotfiles/.screenlayout/right-vertical.sh
+  sh ~/dotfiles/.screenlayout/right-vertical.sh
 else
-  echo "Error: $SCREENLAYOUT not set properly"
+  echo "Error: ./current-layout not formatted properly"
+  sh ~/dotfiles/.screenlayout/right-vertical.sh
 fi
