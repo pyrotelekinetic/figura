4802c4feb3a7287baccb2030b4928ce64fa2c2d7
Carter Ison <carter@isons.org>
2021-02-10 22:13:02 -0700

    fix shebangs in screenlayout scripts

--- .screenlayout/horizontal.sh
+++ .screenlayout/horizontal.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 xrandr --output DP-1 --off --output DP-2 --off --output DP-3 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
 
--- .screenlayout/toggle-layout.sh
+++ .screenlayout/toggle-layout.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 SCREENLAYOUT=$(<~/dotfiles/.screenlayout/current-layout)
 
