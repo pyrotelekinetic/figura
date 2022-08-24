321843e29858a1cc5aab63a08c7a0d1cb4f1fcaf
Carter Ison <carter@isons.org>
2022-08-24 10:34:37 -0700

    remove unused '.screenlayout/'

--- .screenlayout/horizontal.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-xrandr --output DP-1 --off --output DP-2 --off --output DP-3 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
-
-xsetbg -at 0,0 ~/dotfiles/.screenlayout/1080x1920-bang.jpg -at 1920,0 ~/dotfiles/.screenlayout/1080x1920-bang.jpg
-
-echo "horizontal" > ~/dotfiles/.screenlayout/current-layout
--- .screenlayout/right-vertical.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-xrandr --output DP-1 --off --output DP-2 --off --output DP-3 --mode 1920x1080 --pos 1920x0 --rotate left --output HDMI-1 --primary --mode 1920x1080 --pos 0x420 --rotate normal
-
-xsetbg -at 0,420 ~/dotfiles/.screenlayout/1080x1920-bang.jpg -at 1920,0 ~/dotfiles/.screenlayout/1920x1080-spikeandjet.png
-
-echo "right-vertical" > ~/dotfiles/.screenlayout/current-layout
