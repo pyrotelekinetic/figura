f5c2d51fa374114eb8f924c555d33193662edbf5
Carter Ison <carter@isons.org>
2023-04-26 18:57:16 -0700

    Remove unhelpful indent level
    If anything it made the file harder to read.

--- home.nix
+++ home.nix
@@ -1,92 +1,92 @@
 {pkgs, ...}: {
 
-  home = {
-    username = "cison";
-    homeDirectory = "/home/cison";
+home = {
+  username = "cison";
+  homeDirectory = "/home/cison";
 
-    stateVersion = "22.11";
+  stateVersion = "22.11";
 
-    # Verify Home Manager and Nixpkgs are on same release ver
-    enableNixpkgsReleaseCheck = true;
+  # Verify Home Manager and Nixpkgs are on same release ver
+  enableNixpkgsReleaseCheck = true;
 
-    # Set mouse cursor for gtk and x11
-    pointerCursor = {
-      name = "breeze_cursors";
-      package = pkgs.breeze-gtk;
-      size = 24;
-      gtk.enable = true;
-      x11.enable = true;
-    };
-
-    # Tell things to use wayland
-    sessionVariables = {
-      MOZ_ENABLE_WAYLAND = 1;
-      XDG_CURRENT_DESKTOP = "sway";
-      NIXOS_OZONE_WL="1";
-    };
+  # Set mouse cursor for gtk and x11
+  pointerCursor = {
+    name = "breeze_cursors";
+    package = pkgs.breeze-gtk;
+    size = 24;
+    gtk.enable = true;
+    x11.enable = true;
   };
 
-  # Home Manager managed programs
-  programs = {
-    # Manage HM with HM
-    home-manager.enable = true;
-
-    # Make bash work with HM
-    bash = {
-      enable = true;
-      # I should do this in a better way
-      bashrcExtra = "
-        source ~/.oldbashrc
-      ";
-    };
-
-    gpg = {
-      enable = true;
-      mutableKeys = true;
-      mutableTrust = true;
-      settings = {
-        keyid-format = "none";
-        with-subkey-fingerprint = true;
-      };
-    };
-
-    password-store = {
-      enable = true;
-      settings = {
-        PASSWORD_STORE_DIR = "$HOME/.password-store";
-        PASSWORD_STORE_GENERATED_LENGTH = "64";
-      };
-    };
+  # Tell things to use wayland
+  sessionVariables = {
+    MOZ_ENABLE_WAYLAND = 1;
+    XDG_CURRENT_DESKTOP = "sway";
+    NIXOS_OZONE_WL="1";
   };
+};
 
-  # Home Manager managed services
-  services = {
-    gpg-agent = {
-      enable = true;
-      enableBashIntegration = true;
-      enableScDaemon = true;
-      enableSshSupport = true;
-      sshKeys = [ "3C305675F93CC000802C4DE9F6DDF464ACDD3DE3" ];
-      grabKeyboardAndMouse = true;
-      pinentryFlavor = "qt";
-    };
-  };
+# Home Manager managed programs
+programs = {
+  # Manage HM with HM
+  home-manager.enable = true;
 
-  gtk = {
+  # Make bash work with HM
+  bash = {
     enable = true;
+    # I should do this in a better way
+    bashrcExtra = "
+      source ~/.oldbashrc
+    ";
   };
 
-  qt = {
+  gpg = {
     enable = true;
-    style = {
-      package = pkgs.adwaita-qt;
-      name = "adwaita-dark";
+    mutableKeys = true;
+    mutableTrust = true;
+    settings = {
+      keyid-format = "none";
+      with-subkey-fingerprint = true;
     };
   };
 
-  nixpkgs.config.allowUnfree = true;
+  password-store = {
+    enable = true;
+    settings = {
+      PASSWORD_STORE_DIR = "$HOME/.password-store";
+      PASSWORD_STORE_GENERATED_LENGTH = "64";
+    };
+  };
+};
 
-  home.packages = with pkgs; [
+# Home Manager managed services
+services = {
+  gpg-agent = {
+    enable = true;
+    enableBashIntegration = true;
+    enableScDaemon = true;
+    enableSshSupport = true;
+    sshKeys = [ "3C305675F93CC000802C4DE9F6DDF464ACDD3DE3" ];
+    grabKeyboardAndMouse = true;
+    pinentryFlavor = "qt";
+  };
+};
+
+gtk = {
+  enable = true;
+};
+
+qt = {
+  enable = true;
+  style = {
+    package = pkgs.adwaita-qt;
+    name = "adwaita-dark";
+  };
+};
+
+nixpkgs.config.allowUnfree = true;
+
+home.packages = with pkgs; [
   # Utilities
   coreutils-full
   psmisc
@@ -136,6 +136,6 @@
   prismlauncher
   osu-lazer
   heroic
-  ];
+];
 
 }
