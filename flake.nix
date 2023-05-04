6b968311a3e71203da49020e7823bbb21ee1bf97
Carter Ison <carter@isons.org>
2023-05-04 15:07:05 -0700

    Build home-manager configuration with flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+
+description = "Home Manager configuration of Carter";
+
+inputs = {
+  nixpkgs = {
+    type = "github";
+    owner = "NixOS";
+    repo = "nixpkgs";
+    ref = "nixos-22.11";
+  };
+  home-manager = {
+    type = "github";
+    owner = "nix-community";
+    repo = "home-manager";
+    ref = "release-22.11";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+};
+
+outputs = { nixpkgs, home-manager, ... }: {
+  homeConfigurations.cison = home-manager.lib.homeManagerConfiguration {
+    pkgs = import nixpkgs {
+      config.allowUnfree = true;
+      system = "x86_64-linux";
+    };
+    modules = [
+      ./home.nix
+    ];
+  };
+};
+
+}
