30ef61695d6da137d0d6aa68e5567eb1f229c8ac
Carter Ison <carter@isons.org>
2023-06-22 01:15:51 -0700

    Use 'or' instead of 'if'
    'or' is more idiomatic in this case.

--- mkSystem.nix
+++ mkSystem.nix
@@ -6,7 +6,7 @@ inputs: { host, system, homeOpts ? {} }: with inputs; {
     ./mundus
     (self + "/mundus/" + host)
     {
-      system.configurationRevision = if self?rev then self.rev else "DIRTY";
+      system.configurationRevision = self.rev or "DIRTY";
       networking.hostName = host;
       nix.registry.nixpkgs.flake = nixpkgs;
       environment.etc."nix/inputs/nixpkgs".source = nixpkgs.outPath;
