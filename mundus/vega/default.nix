b176bee3f8c8267a603b74859d59022d1be384af
Carter Ison <carter@isons.org>
2023-06-22 20:23:27 -0700

    Set backlight brightness on vega

--- mundus/vega/default.nix
+++ mundus/vega/default.nix
@@ -7,4 +7,8 @@ imports = [
 # Don't want sshd on a portable machine that connects to lots of different networks
 services.openssh.enable = lib.mkForce false;
 
+# Control screen backlight, must be in video group
+hardware.acpilight.enable = true;
+users.users.cison.extraGroups = [ "video" ];
+
 }
