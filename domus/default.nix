9d11361871d911b7467b07a00a72f9a2bdeec6fa
Carter Ison <carter@isons.org>
2023-07-01 21:39:43 -0700

    Use packaged 'git-prompt.sh' for bash prompt

--- domus/default.nix
+++ domus/default.nix
@@ -51,14 +51,11 @@ programs = {
   home-manager.enable = true;
 
   # Make bash work with HM
-  bash = let
-    git-prompt = pkgs.writeScript "git-prompt.sh"
-      ( builtins.readFile ./share/git-prompt.sh );
-  in {
+  bash = {
     enable = true;
     initExtra = ''
       # Set prompt
-      source ${git-prompt}
+      source ${pkgs.git}/share/bash-completion/completions/git-prompt.sh
       GIT_PS1_SHOWDIRTYSTATE=true
       PS1="\[\e[0;37m\][\[\e[0;95m\]\u\[\e[0;34m\]@\[\e[0;95m\]\h \[\e[1;32m\]\w\[\e[0;37m\]\[\e[33m\]\$(__git_ps1 ' (%s)')\[\e[0;37m\]]\$\[\e[0m\] "
 
