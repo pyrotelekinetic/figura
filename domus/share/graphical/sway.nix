a821ba61912b32dd0c0d31ecd7a8ce636a63cca0
Carter Ison <carter@isons.org>
2023-07-12 11:36:29 -0700

    Move workspace-monitor assignments to sol config

--- domus/share/graphical/sway.nix
+++ domus/share/graphical/sway.nix
@@ -10,52 +10,6 @@ config = {
   wayland.windowManager.sway = {
     config = {
       defaultWorkspace = "workspace number 1";
-      workspaceOutputAssign =
-        let
-          primary = "DP-3";
-          secondary = "HDMI-A-1";
-        in [
-          {
-            output = primary;
-            workspace = "1";
-          }
-          {
-            output = primary;
-            workspace = "3";
-          }
-          {
-            output = primary;
-            workspace = "5";
-          }
-          {
-            output = primary;
-            workspace = "7";
-          }
-          {
-            output = primary;
-            workspace = "9";
-          }
-          {
-            output = secondary;
-            workspace = "2";
-          }
-          {
-            output = secondary;
-            workspace = "4";
-          }
-          {
-            output = secondary;
-            workspace = "6";
-          }
-          {
-            output = secondary;
-            workspace = "8";
-          }
-          {
-            output = secondary;
-            workspace = "0";
-          }
-        ];
       bars = [ {
         command = "swaybar";
         colors = with colors; {
--- domus/sol/sway.nix
+++ domus/sol/sway.nix
@@ -1,8 +1,55 @@
-{ ... }: {
+{ ... }: let
+  primary = "DP-3";
+  secondary = "HDMI-A-1";
+in {
 
-wayland.windowManager.sway.config.output = {
-  DP-3.pos = "0 0";
-  HDMI-A-1.pos = "1920 338";
+wayland.windowManager.sway.config = {
+  output = {
+    ${primary}.pos = "0 0";
+    ${secondary}.pos = "1920 338";
+  };
+  workspaceOutputAssign = [
+      {
+        output = primary;
+        workspace = "1";
+      }
+      {
+        output = primary;
+        workspace = "3";
+      }
+      {
+        output = primary;
+        workspace = "5";
+      }
+      {
+        output = primary;
+        workspace = "7";
+      }
+      {
+        output = primary;
+        workspace = "9";
+      }
+      {
+        output = secondary;
+        workspace = "2";
+      }
+      {
+        output = secondary;
+        workspace = "4";
+      }
+      {
+        output = secondary;
+        workspace = "6";
+      }
+      {
+        output = secondary;
+        workspace = "8";
+      }
+      {
+        output = secondary;
+        workspace = "0";
+      }
+    ];
 };
 
 }
