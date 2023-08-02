6f17965abe889221e5ffc854e30c4434d6889a62
Carter Ison <carter@isons.org>
2023-08-01 19:11:30 -0700

    Deploy test secret with sops-nix

--- mundus/default.nix
+++ mundus/default.nix
@@ -1,8 +1,18 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }: {
 
 imports = [
   ./pyrosite.nix
 ];
+
+sops = {
+  defaultSopsFile = ../secrets.yaml;
+  age.sshKeyPaths = [];
+  gnupg ={
+    home = config.home-manager.users.cison.programs.gpg.homedir;
+    sshKeyPaths = [];
+  };
+  secrets.testSecret = {};
+};
 
 hardware = {
   opengl = {
@@ -69,6 +79,7 @@ users.users.cison = {
     "networkmanager"
     "wheel"
     "disk"
+    "keys"
   ];
   openssh.authorizedKeys.keys = [
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC4X2NdKZeEjmuezkLudU8HGnPnxCMC7g7LkWJdr+Rn9K/VuJOHSHXWeWXtxiAQiPjRiE71FflXLmTTUuPZFM6GCKSxd3uZ/Sx6Cw4qQdWoIfuNdJR+Gl89uEIpO2URBHmpxHRRB58qTLyBz86giU2jIq18jCvu9OdubvqBUavHmUrz63g13TNRAwbOBOqt8Mx3RWfvw0+KXOD8BykWLSKsOfOaqErAcAzEuhGHZ1boR+HcwgHis9Q7/YWSfojne3wqftSNHlSjYKDziI9cC3CPSsjidMZkUuhL6vP7gzPln9H0sktyVmrQXxlzMD4zc9IQLiODemWjOPVuYn1alOsFvfBIiQIB3maU5PV33uwba1i1rYgX8c+nSscokdD+F4S4ILlI7vFSYZVLgun/DPtshNiewT7y0uJgNmiJUzU75EO0uKpfnw8Sr/MoisiIglLDzUYjTOv7sHI0YXGRYrwpRkokuxTDZVoHWodiZp7Jej8VpEMzu5zu/AG5c00OWCTSD33bgpCahhSqy/vPBuqqmkBwML7hN/mIw+U/EUuT5uE392ktfw2uJuLQWwVPAkytaNCWgsmKzED0fhjoJVY1T9nbzP50iAJf+uvnFvJ+jJD+nx97ZghsmwivfRbFLKC2iH4eBZjjcWhDZ8aSqyFnInuaiY4YIkLomRdJbnhaOQ== (none)"
