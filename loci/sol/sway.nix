faf64d0d50c6245937bcbc1716920d491a469cc4
Carter Ison <carter@isons.org>
2023-08-28 13:09:07 -0700

    sol: Display all notifications on primary monitor

--- loci/sol/sway.nix
+++ loci/sol/sway.nix
@@ -3,54 +3,57 @@
   secondary = "HDMI-A-1";
 in {
 
-home-manager.users.cison.wayland.windowManager.sway.config = {
-  output = {
-    ${primary}.pos = "0 0";
-    ${secondary}.pos = "1920 0";
+home-manager.users.cison = {
+  wayland.windowManager.sway.config = {
+    output = {
+      ${primary}.pos = "0 0";
+      ${secondary}.pos = "1920 0";
+    };
+    workspaceOutputAssign = [
+        {
+          output = primary;
+          workspace = "1";
+        }
+        {
+          output = primary;
+          workspace = "3";
+        }
+        {
+          output = primary;
+          workspace = "5";
+        }
+        {
+          output = primary;
+          workspace = "7";
+        }
+        {
+          output = primary;
+          workspace = "9";
+        }
+        {
+          output = secondary;
+          workspace = "2";
+        }
+        {
+          output = secondary;
+          workspace = "4";
+        }
+        {
+          output = secondary;
+          workspace = "6";
+        }
+        {
+          output = secondary;
+          workspace = "8";
+        }
+        {
+          output = secondary;
+          workspace = "0";
+        }
+      ];
+      input."type:tablet_tool".map_to_output = primary;
   };
-  workspaceOutputAssign = [
-      {
-        output = primary;
-        workspace = "1";
-      }
-      {
-        output = primary;
-        workspace = "3";
-      }
-      {
-        output = primary;
-        workspace = "5";
-      }
-      {
-        output = primary;
-        workspace = "7";
-      }
-      {
-        output = primary;
-        workspace = "9";
-      }
-      {
-        output = secondary;
-        workspace = "2";
-      }
-      {
-        output = secondary;
-        workspace = "4";
-      }
-      {
-        output = secondary;
-        workspace = "6";
-      }
-      {
-        output = secondary;
-        workspace = "8";
-      }
-      {
-        output = secondary;
-        workspace = "0";
-      }
-    ];
-    input."type:tablet_tool".map_to_output = primary;
+  services.mako.output = primary;
 };
 
 }
