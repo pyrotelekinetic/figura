3a9aa077469193d0540faebc5f51669aadf4f402
Carter Ison <carter@isons.org>
2023-09-10 14:42:40 -0700

    vega: Enable sshd
    I changed my mind about not wanting sshd running. The convenience
    outweighs the possible security weakness. Besides, I only have key auth
    enabled.

--- loci/vega/default.nix
+++ loci/vega/default.nix
@@ -8,9 +8,6 @@ imports = [
 
 system.stateVersion = "23.05";
 
-# Don't want sshd on a portable machine that connects to lots of different networks
-services.openssh.enable = lib.mkForce false;
-
 # Control screen backlight, must be in video group
 hardware.acpilight.enable = true;
 users.users.cison.extraGroups = [ "video" ];
