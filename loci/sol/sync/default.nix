15f73e88697f5cd5bea1be59ead3899657ca7646
Carter Ison <carter@isons.org>
2023-12-08 10:03:31 -0700

    loci/sol: Set syncthing configDir
    Replace string literal with config option for homeDir.

--- loci/sol/sync/default.nix
+++ loci/sol/sync/default.nix
@@ -8,7 +8,8 @@ imports = [
 services.syncthing = {
   enable = true;
   user = "cison";
-  dataDir = "/home/cison";
+  dataDir = config.users.users.cison.home;
+  configDir = config.users.users.cison.home + "/.config/syncthing";
   key = config.sops.secrets."syncthing/key.pem".path;
   cert = config.sops.secrets."syncthing/cert.pem".path;
 };
