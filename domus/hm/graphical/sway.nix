68367e1ca3c38d2f4c5678cd6b3690817d3f48b9
Carter Ison <carter@isons.org>
2024-04-09 09:19:23 -0700

    domus/hm/graphical: Fix error in sway mouse config
    Mice will have flat acceleration curve for real.

--- domus/hm/graphical/sway.nix
+++ domus/hm/graphical/sway.nix
@@ -92,7 +92,7 @@ config = {
           xkb_options = "compose:ralt";
         };
         "type:touchpad".natural_scroll = "enabled";
-        "type:mouse".accel_profile = "flat";
+        "type:pointer".accel_profile = "flat";
         "5426:97:Razer_Atheris_Mouse".pointer_accel = "-1";
       };
 
