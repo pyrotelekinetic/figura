0ad86656af0f5155f111b2f252dc89831c315d89
Carter Ison <carter@isons.org>
2024-04-26 22:41:15 -0700

    loci/sol: Fix boot-chime
    Gerg's skill issue "fix" included a skill issue.

--- loci/sol/boot-chime.nix
+++ loci/sol/boot-chime.nix
@@ -21,7 +21,7 @@ systemd.services.boot-chime = {
   ];
 
   script = ''
-    RIFF=$(shuf -n 1 -e "$RIFFS/riffs/*")
+    RIFF=$(shuf -n 1 -e $RIFFS/riffs/*)
     ffmpeg -i "$RIFF" -c pcm_s32le -f wav - | \
     aplay --device=hdmi:CARD=HDMI,DEV=3 -r44100 -f S32_LE -
   '';
