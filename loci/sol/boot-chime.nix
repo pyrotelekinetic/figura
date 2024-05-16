f29094d927b24784af872f3f1b5118c03b32cc12
Carter Ison <carter@isons.org>
2024-05-16 11:31:43 -0700

    loci/sol/boot-chime: Only use shorter riffs

--- loci/sol/boot-chime.nix
+++ loci/sol/boot-chime.nix
@@ -9,11 +9,11 @@ systemd.services.boot-chime = {
   };
 
   environment.RIFFS = pkgs.fetchFromGitHub {
-      owner = "pyrotelekinetic";
-      repo = "seinfeld-riffs";
-      rev = "e38fc089b68fbca5796ac4bdb55f34a5e005b1fd";
-      hash = "sha256-FkF5UmKsSkbjrPqYAt7xhOn4aiUMBxotl3JXzweSaNI=";
-    };
+    owner = "pyrotelekinetic";
+    repo = "seinfeld-riffs";
+    rev = "40e0e69bbb2fbaae5829873fea7bb2d8f3bb4531";
+    hash = "sha256-7PxTcy/jvRoIT+8eyG2oMaYI2Unz+P06laWKiJqPUnU=";
+  };
 
   path = [
     pkgs.alsa-utils
@@ -21,7 +21,7 @@ systemd.services.boot-chime = {
   ];
 
   script = ''
-    RIFF=$(shuf -n 1 -e $RIFFS/riffs/*)
+    RIFF=$(shuf -n 1 -e $RIFFS/riffs/short/*)
     ffmpeg -i "$RIFF" -c pcm_s32le -f wav - | \
     aplay --device=hdmi:CARD=HDMI,DEV=3 -r44100 -f S32_LE -
   '';
