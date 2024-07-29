65cd932bbd4aa34a10090c65550a3217d5858a77
Carter Ison <carter@isons.org>
2024-07-29 14:57:29 -0700

    sol/sway: Add output and workspace for crt display

--- loci/sol/sway.nix
+++ loci/sol/sway.nix
@@ -1,6 +1,7 @@
 { lib, ... }: let
   primary = "ViewSonic Corporation VA2265 SERIES U99153701230";
   secondary = "Ancor Communications Inc ASUS VS228 H5LMTF142883";
+  crt = "HJW MACROSILICON 0x0002E9BD";
 in {
 
 home-manager.users.cison = {
@@ -14,10 +15,23 @@ home-manager.users.cison = {
         position = "-1920 0";
         subpixel = "rgb";
       };
+      ${crt} = {
+        modeline = "63.02  640 680 752 864  480 481 484 521  -hsync +vsync";
+        position = "0 2000";
+        subpixel = "none";
+      };
+    };
+    keybindings = let
+      mod = "Mod4";
+    in {
+      "${mod}+Grave" = "workspace crt";
+      "${mod}+Shift+Grave" = "move container to workspace crt";
     };
     workspaceOutputAssign = let
       assign = x: lib.map (n: { output = x; workspace = toString n; });
-    in (assign primary [1 3 5 7 9]) ++ (assign secondary [2 4 6 8 0]);
+    in (assign primary [1 3 5 7 9])
+    ++ (assign secondary [2 4 6 8 0])
+    ++ (assign crt ["crt"]);
     input."type:tablet_tool".map_to_output = ''"${primary}"'';
   };
   services.mako.output = primary;
