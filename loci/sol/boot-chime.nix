1968cc45e1b982abb4cf3828b1120f2a43eff615
Carter Ison <carter@isons.org>
2024-08-13 16:14:07 -0700

    sol/boot-chime: Use 'ffplay' instead of piping 'ffmpeg'

--- loci/sol/boot-chime.nix
+++ loci/sol/boot-chime.nix
@@ -8,22 +8,24 @@ systemd.services.boot-chime = {
     RemainAfterExit = false;
   };
 
-  environment.RIFFS = pkgs.fetchFromGitHub {
-    owner = "pyrotelekinetic";
-    repo = "seinfeld-riffs";
-    rev = "40e0e69bbb2fbaae5829873fea7bb2d8f3bb4531";
-    hash = "sha256-7PxTcy/jvRoIT+8eyG2oMaYI2Unz+P06laWKiJqPUnU=";
+  environment = {
+    RIFFS = pkgs.fetchFromGitHub {
+      owner = "pyrotelekinetic";
+      repo = "seinfeld-riffs";
+      rev = "40e0e69bbb2fbaae5829873fea7bb2d8f3bb4531";
+      hash = "sha256-7PxTcy/jvRoIT+8eyG2oMaYI2Unz+P06laWKiJqPUnU=";
+    };
+    SDL_AUDIODRIVER = "alsa";
+    AUDIODEV = "hdmi:CARD=HDMI,DEV=3";
   };
 
   path = [
-    pkgs.alsa-utils
     pkgs.ffmpeg
   ];
 
   script = ''
     RIFF=$(shuf -n 1 -e $RIFFS/riffs/short/*)
-    ffmpeg -i "$RIFF" -c pcm_s32le -f wav - | \
-    aplay --device=hdmi:CARD=HDMI,DEV=3 -r44100 -f S32_LE -
+    ffplay "$RIFF" -hide_banner -autoexit -vn -nodisp
   '';
 };
 
