9ef9704c33ffb72ee71a6d3603c9458a052f60c6
Carter Ison <carter@isons.org>
2025-01-30 20:32:11 -0700

    alsafi: Initialize host

---
+++ loci/alsafi/default.nix
@@ -0,0 +1,37 @@
+{ lib, ... }: {
+
+imports = [
+  ./hardware.nix
+];
+
+head.headless = true;
+
+boot = {
+  loader = {
+    systemd-boot = {
+      enable = true;
+      configurationLimit = 1; # I can't make a selection at boot anyway
+      editor = false;
+    };
+    efi = {
+      efiSysMountPoint = "/efi";
+      canTouchEfiVariables = true;
+    };
+  };
+};
+
+# alsafi is a vm... no SMART support
+services.smartd.enable = lib.mkForce false;
+
+users.mutableUsers = false;
+users.users.cison.initialHashedPassword = lib.mkForce null;
+# Passwordless, so allow polkit actions without auth as well
+security.polkit.extraConfig = ''
+  polkit.addRule(function(action, subject) {
+    if (subject.isInGroup("wheel")) return polkit.Result.YES;
+  });
+'';
+
+system.stateVersion = "24.11";
+
+}
