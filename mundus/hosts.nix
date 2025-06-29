79594f78cae710701747e5c27abe80e576e18bc4
Carter Ison <carter@isons.org>
2025-06-29 15:15:24 -0700

    alsafi: Update public ip and ssh host key

--- mundus/hosts.nix
+++ mundus/hosts.nix
@@ -2,7 +2,7 @@
   ips = {
     wan = "184.179.188.130";
     gate = "192.168.1.1";
-    alsafi = "141.148.191.58";
+    alsafi = "141.148.160.13";
     luna = "192.168.1.2";
     sol = "192.168.1.8";
     halley = "192.168.1.9";
@@ -42,7 +42,7 @@ programs.ssh = {
   knownHosts = {
     alsafi = {
       extraHostNames = [ ips.alsafi ];
-      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAiHyo9M7KDeoeAd0iO35Ww8XavJ1vXk2F0IcrADY5lv";
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKUt/0Kt/6kPDbau2ixCWEKrjF7lRdXAu2qMEgKzKixW";
     };
     luna = {
       extraHostNames = [
