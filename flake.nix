79d3b741e78cbe92f49902894f3d241f40328c3c
Carter Ison <carter@isons.org>
2025-07-01 21:45:19 -0700

    vega: Bump lanzaboote and update key path

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@ inputs = {
     inputs.nixpkgs.follows = "nixpkgs";
   };
   lanzaboote = {
-    url = "github:nix-community/lanzaboote?ref=v0.4.1";
+    url = "github:nix-community/lanzaboote?ref=v0.4.2";
     inputs.nixpkgs.follows = "nixpkgs";
   };
   lix-module = {
